fb53b28c828ceda484cdc203b0d08afd6199a959
Silas De Vuyst <silas.devuyst@student.hogent.be>
2024-05-01 13:17:03 +0200

    added icon

--- js/main.js
+++ js/main.js
@@ -41,3 +41,21 @@ $resizeMap = function() {
         $("#map").addClass("full-width");
     }   
 }
+
+// Update ico on dark mode / light mode
+lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+
+function onUpdate() {
+    if (matcher.matches) {
+      lightSchemeIcon.remove();
+      document.head.append(darkSchemeIcon);
+    } else {
+      document.head.append(lightSchemeIcon);
+      darkSchemeIcon.remove();
+    }
+  }
+  
+  matcher = window.matchMedia('(prefers-color-scheme: dark)');
+  matcher.addListener(onUpdate);
+  onUpdate();
