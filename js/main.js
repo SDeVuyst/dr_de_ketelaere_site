fd82e641db6e9cd26d8f0734d6304a06bb0dec9c
Silas De Vuyst <silas.devuyst@student.hogent.be>
2024-05-01 11:44:08 +0200

    docker + images

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     $addCorrectClassesForAfspraakButton();
+    $addbpoints();
+    $resizeMap();
 });
 
 $(window).on('resize', function() {
     $addCorrectClassesForAfspraakButton();
+    $addbpoints();
+    $resizeMap();
 })
 
 $addCorrectClassesForAfspraakButton = function() {
@@ -16,4 +20,24 @@ $addCorrectClassesForAfspraakButton = function() {
         $('#afspraakbutton').addClass('hide_button');
         $('#afspraakbutton').removeClass('fill');
     }
+}
+
+$addbpoints = function() {
+    if ($(window).width() > 992) {
+        $(".bpoint").each(function() {
+            $(this).removeClass("w-100");
+        })
+    } else {
+        $(".bpoint").each(function() {
+            $(this).addClass("w-100");
+        })
+    }   
+}
+
+$resizeMap = function() {
+    if ($(window).width() > 992) {
+        $("#map").removeClass("full-width");
+    } else {
+        $("#map").addClass("full-width");
+    }   
 }
