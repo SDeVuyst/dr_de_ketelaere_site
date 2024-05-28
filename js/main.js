ce1fca2b8395b1d731c74c77ee8fb6524825d759
Silas De Vuyst <silasdevuyst@hotmail.com>
2024-05-28 21:04:00 +0200

    updated favicon

--- js/main.js
+++ js/main.js
@@ -42,25 +42,6 @@ $resizeMap = function() {
     }   
 }
 
-// Update ico on dark mode / light mode
-lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
-
-function onUpdate() {
-    if (matcher.matches) {
-      lightSchemeIcon.remove();
-      document.head.append(darkSchemeIcon);
-    } else {
-      document.head.append(lightSchemeIcon);
-      darkSchemeIcon.remove();
-    }
-  }
-  
-  matcher = window.matchMedia('(prefers-color-scheme: dark)');
-  matcher.addListener(onUpdate);
-  onUpdate();
-
-
 const buttonWrapper = document.querySelector('.button-container');
 
 buttonWrapper.addEventListener('mouseenter', function() {
